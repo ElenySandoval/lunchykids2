c1124d3976e1ad1a5b13cdc097e0c32c1ccae6df
ElenySandoval <elenyvalentinasan@gmail.com>
2022-10-22 13:25:12 -0500

    Cargando Base de Datos

--- assets/bbdd_proyecto/DataBase.php
+++ assets/bbdd_proyecto/DataBase.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 $conn = mysqli_connect(
-  'localhost',
-  'root',
-  '',
-  'lunchy_kids'
+  'baablmkyh3jisolknsbl-mysql.services.clever-cloud.com',
+  'uf6fdgtbevwmlokn',
+  'L521tY4Zg2HE5ckCh7Nz',
+  'baablmkyh3jisolknsbl'
 
 ) or die(mysqli_erro($mysqli));
 
